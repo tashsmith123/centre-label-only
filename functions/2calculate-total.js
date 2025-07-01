3fe14dbea3fdef6cdab1459e38f5592ecf242330
tashsmith123 <smith.natassja@googlemail.com>
2025-07-01 15:37:28 +0100

    Calculating fully correctly
    dynamic variables are not correct but that's just data entry

--- functions/2calculate-total.js
+++ functions/2calculate-total.js
@@ -11,7 +11,10 @@ console.log("const is working");
   let priceTotal = 0;
   let summary = "";
   let totalPrintedPartsCost = 0;
+  let printingFee = 0;
   let hasPrintedLabel = false;
+  let hasPrintedInner = false;
+  let hasPrintedOuter = false;
   const checkboxStates = getCheckboxValues();
   console.log("Checkbox states:", checkboxStates);
 
@@ -108,6 +111,389 @@ console.log(`Variant ${i + 1} Label type:`, currentLabelType);
   labelCost = 0;
 }
 
+//inners 
+let innersCost = 0;
+
+// If there is only 1 variant, always calculate labels
+if (qtyInputs.length === 1) {
+  const currentInnersType = innerSelects[i]?.value;
+console.log(`Variant ${i + 1} Inners type:`, currentInnersType);
+  if (currentInnersType === "None") {
+    innersCost = 0.00;
+  } else if (currentInnersType === "Black Gloss Disco 0 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Black Gloss Disco 2 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Black Matt Disco 0 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Black Matt Disco 2 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "White Gloss Disco 0 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "White Gloss Disco 2 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "White Matt Disco 0 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "White Matt Disco 2 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Kraft Disco 0 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Kraft Disco 2 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Black Paper") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "White Paper") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Black Poly") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "White Poly") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Rounded Anti-Static") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Supplied") {
+    innersCost = 0.10;
+  } else if (currentInnersType === "Printed") {
+    hasPrintedInner = true;
+    innersCost =
+      qty >= 10000 ? 0.06 :
+      qty >= 9000  ? 0.06 :
+      qty >= 8000  ? 0.06 :
+      qty >= 7000  ? 0.07 :
+      qty >= 6000  ? 0.06 :
+      qty >= 5000  ? 0.06 :
+      qty >= 4000  ? 0.06 :
+      qty >= 3000  ? 0.06 :
+      qty >= 2500  ? 0.05 :
+      qty >= 2000  ? 0.05 :
+      qty >= 1500  ? 0.05 :
+      qty >= 1000  ? 0.04 :
+      qty >= 750   ? 0.03 :
+      qty >= 500   ? 0.09 :
+      qty >= 400   ? 0.07 :
+      qty >= 300   ? 0.03 :
+      qty >= 250   ? 0.03 :
+      qty >= 200   ? 0.04 :
+      qty >= 100   ? 0.04 :
+      0.000;
+  }
+
+} else if (checkboxStates[variantId]) {
+  // Multiple variants, checkbox is checked for this one
+  const currentInnersType = innerSelects[i]?.value;
+
+  if (currentInnersType === "None") {
+    innersCost = 0.00;
+  } else if (currentInnersType === "Black Gloss Disco 0 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Black Gloss Disco 2 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Black Matt Disco 0 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Black Matt Disco 2 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "White Gloss Disco 0 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "White Gloss Disco 2 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "White Matt Disco 0 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "White Matt Disco 2 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Kraft Disco 0 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Kraft Disco 2 Holes") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Black Paper") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "White Paper") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Black Poly") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "White Poly") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Rounded Anti-Static") {
+    innersCost = 0.35;
+  } else if (currentInnersType === "Supplied") {
+    innersCost = 0.10;
+  } else if (currentInnersType === "Printed") {
+    hasPrintedInner = true;
+    innersCost =
+      qty >= 10000 ? 0.06 :
+      qty >= 9000  ? 0.06 :
+      qty >= 8000  ? 0.06 :
+      qty >= 7000  ? 0.07 :
+      qty >= 6000  ? 0.06 :
+      qty >= 5000  ? 0.06 :
+      qty >= 4000  ? 0.06 :
+      qty >= 3000  ? 0.06 :
+      qty >= 2500  ? 0.05 :
+      qty >= 2000  ? 0.05 :
+      qty >= 1500  ? 0.05 :
+      qty >= 1000  ? 0.04 :
+      qty >= 750   ? 0.03 :
+      qty >= 500   ? 0.09 :
+      qty >= 400   ? 0.07 :
+      qty >= 300   ? 0.03 :
+      qty >= 250   ? 0.03 :
+      qty >= 200   ? 0.04 :
+      qty >= 100   ? 0.04 :
+      0.000;
+  }
+} else {
+  // Multiple variants, checkbox not checked for this one: no label cost
+  innersCost = 0;
+}
+
+let OutersCost = 0;
+
+// If there is only 1 variant, always calculate labels
+if (qtyInputs.length === 1) {
+  const currentOutersType = outerSelects[i]?.value;
+console.log(`Variant ${i + 1} Outers type:`, currentOutersType);
+  if (currentOutersType === "None") {
+    OutersCost = 0.00;
+  } else if (currentOutersType === "Black Gloss 0") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "Black Gloss 2") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "Black Matt 0") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "Black Matt 2") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "White Gloss 0") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "White Gloss 2") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "White Matt 0") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "White Matt 2") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "Kraft 0") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "Kraft 2") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "Black Gloss 3mm 0") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "Black Gloss 3mm 2") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "Black Matt 3mm 0") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "Black Matt 3mm 2") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "White Gloss 3mm 0") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "White Gloss 3mm 2") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "White Matt 3mm 0") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "White Matt 3mm 2") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "Kraft 3mm 0") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "Kraft 3mm 2") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "Picture Disc") {
+    OutersCost = 0.70;
+  } else if (currentOutersType === "Supplied") {
+    OutersCost = 0.10;
+  
+  } else if (currentOutersType === "Printed 3mm") {
+    hasPrintedOuter = true;
+    OutersCost =
+      qty >= 10000 ? 0.06 :
+      qty >= 9000  ? 0.06 :
+      qty >= 8000  ? 0.06 :
+      qty >= 7000  ? 0.07 :
+      qty >= 6000  ? 0.06 :
+      qty >= 5000  ? 0.06 :
+      qty >= 4000  ? 0.06 :
+      qty >= 3000  ? 0.06 :
+      qty >= 2500  ? 0.05 :
+      qty >= 2000  ? 0.05 :
+      qty >= 1500  ? 0.05 :
+      qty >= 1000  ? 0.04 :
+      qty >= 750   ? 0.03 :
+      qty >= 500   ? 0.09 :
+      qty >= 400   ? 0.07 :
+      qty >= 300   ? 0.03 :
+      qty >= 250   ? 0.03 :
+      qty >= 200   ? 0.04 :
+      qty >= 100   ? 0.04 :
+      0.000;
+  } else if (currentOutersType === "Printed 5mm") {
+    hasPrintedOuter = true;
+    OutersCost =
+      qty >= 10000 ? 0.06 :
+      qty >= 9000  ? 0.06 :
+      qty >= 8000  ? 0.06 :
+      qty >= 7000  ? 0.07 :
+      qty >= 6000  ? 0.06 :
+      qty >= 5000  ? 0.06 :
+      qty >= 4000  ? 0.06 :
+      qty >= 3000  ? 0.06 :
+      qty >= 2500  ? 0.05 :
+      qty >= 2000  ? 0.05 :
+      qty >= 1500  ? 0.05 :
+      qty >= 1000  ? 0.04 :
+      qty >= 750   ? 0.03 :
+      qty >= 500   ? 0.09 :
+      qty >= 400   ? 0.07 :
+      qty >= 300   ? 0.03 :
+      qty >= 250   ? 0.03 :
+      qty >= 200   ? 0.04 :
+      qty >= 100   ? 0.04 :
+      0.000;
+  } else if (currentOutersType === "Gatefold") {
+    hasPrintedOuter = true;
+    OutersCost =
+      qty >= 10000 ? 0.06 :
+      qty >= 9000  ? 0.06 :
+      qty >= 8000  ? 0.06 :
+      qty >= 7000  ? 0.07 :
+      qty >= 6000  ? 0.06 :
+      qty >= 5000  ? 0.06 :
+      qty >= 4000  ? 0.06 :
+      qty >= 3000  ? 0.06 :
+      qty >= 2500  ? 0.05 :
+      qty >= 2000  ? 0.05 :
+      qty >= 1500  ? 0.05 :
+      qty >= 1000  ? 0.04 :
+      qty >= 750   ? 0.03 :
+      qty >= 500   ? 0.09 :
+      qty >= 400   ? 0.07 :
+      qty >= 300   ? 0.03 :
+      qty >= 250   ? 0.03 :
+      qty >= 200   ? 0.04 :
+      qty >= 100   ? 0.04 :
+      0.000;
+    
+  }
+
+} else if (checkboxStates[variantId]) {
+  // Multiple variants, checkbox is checked for this one
+  const currentOutersType = outerSelects[i]?.value;
+
+  if (currentOutersType === "None") {
+    OutersCost = 0.00;
+  } else if (currentOutersType === "Black Gloss 0") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "Black Gloss 2") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "Black Matt 0") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "Black Matt 2") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "White Gloss 0") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "White Gloss 2") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "White Matt 0") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "White Matt 2") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "Kraft 0") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "Kraft 2") {
+    OutersCost = 0.35;
+  } else if (currentOutersType === "Black Gloss 3mm 0") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "Black Gloss 3mm 2") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "Black Matt 3mm 0") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "Black Matt 3mm 2") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "White Gloss 3mm 0") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "White Gloss 3mm 2") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "White Matt 3mm 0") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "White Matt 3mm 2") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "Kraft 3mm 0") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "Kraft 3mm 2") {
+    OutersCost = 0.39;
+  } else if (currentOutersType === "Picture Disc") {
+    OutersCost = 0.70;
+  } else if (currentOutersType === "Supplied") {
+    OutersCost = 0.10;
+  
+  } else if (currentOutersType === "Printed 3mm") {
+    hasPrintedOuter = true;
+    OutersCost =
+      qty >= 10000 ? 0.06 :
+      qty >= 9000  ? 0.06 :
+      qty >= 8000  ? 0.06 :
+      qty >= 7000  ? 0.07 :
+      qty >= 6000  ? 0.06 :
+      qty >= 5000  ? 0.06 :
+      qty >= 4000  ? 0.06 :
+      qty >= 3000  ? 0.06 :
+      qty >= 2500  ? 0.05 :
+      qty >= 2000  ? 0.05 :
+      qty >= 1500  ? 0.05 :
+      qty >= 1000  ? 0.04 :
+      qty >= 750   ? 0.03 :
+      qty >= 500   ? 0.09 :
+      qty >= 400   ? 0.07 :
+      qty >= 300   ? 0.03 :
+      qty >= 250   ? 0.03 :
+      qty >= 200   ? 0.04 :
+      qty >= 100   ? 0.04 :
+      0.000;
+  } else if (currentOutersType === "Printed 5mm") {
+    hasPrintedOuter = true;
+    OutersCost =
+      qty >= 10000 ? 0.06 :
+      qty >= 9000  ? 0.06 :
+      qty >= 8000  ? 0.06 :
+      qty >= 7000  ? 0.07 :
+      qty >= 6000  ? 0.06 :
+      qty >= 5000  ? 0.06 :
+      qty >= 4000  ? 0.06 :
+      qty >= 3000  ? 0.06 :
+      qty >= 2500  ? 0.05 :
+      qty >= 2000  ? 0.05 :
+      qty >= 1500  ? 0.05 :
+      qty >= 1000  ? 0.04 :
+      qty >= 750   ? 0.03 :
+      qty >= 500   ? 0.09 :
+      qty >= 400   ? 0.07 :
+      qty >= 300   ? 0.03 :
+      qty >= 250   ? 0.03 :
+      qty >= 200   ? 0.04 :
+      qty >= 100   ? 0.04 :
+      0.000;
+  } else if (currentOutersType === "Gatefold") {
+    hasPrintedOuter = true;
+    OutersCost =
+      qty >= 10000 ? 0.06 :
+      qty >= 9000  ? 0.06 :
+      qty >= 8000  ? 0.06 :
+      qty >= 7000  ? 0.07 :
+      qty >= 6000  ? 0.06 :
+      qty >= 5000  ? 0.06 :
+      qty >= 4000  ? 0.06 :
+      qty >= 3000  ? 0.06 :
+      qty >= 2500  ? 0.05 :
+      qty >= 2000  ? 0.05 :
+      qty >= 1500  ? 0.05 :
+      qty >= 1000  ? 0.04 :
+      qty >= 750   ? 0.03 :
+      qty >= 500   ? 0.09 :
+      qty >= 400   ? 0.07 :
+      qty >= 300   ? 0.03 :
+      qty >= 250   ? 0.03 :
+      qty >= 200   ? 0.04 :
+      qty >= 100   ? 0.04 :
+      0.000;
+  }
+} else {
+  // Multiple variants, checkbox not checked for this one: no label cost
+  OutersCost = 0;
+}
 
 
 //this is all working
@@ -116,16 +502,23 @@ console.log(`Variant ${i + 1} Label type:`, currentLabelType);
       calculatedQty += qty;
     const cost = qty * vinylPrice;
     const labelTotal = qty * labelCost;
+    const innersTotal = qty * innersCost;
+    const outersTotal = qty * OutersCost;
     priceTotal += cost;
     priceTotal += labelTotal;
-    totalPrintedPartsCost += labelTotal;
+    priceTotal += innersTotal;
+    priceTotal += outersTotal;
+
+    totalPrintedPartsCost += labelTotal + innersTotal + outersTotal;
    
 
     summary += `<h4>Variant ${i + 1}</h4>`;
 summary += `<p>Quantity: ${qty}</p>`;
 summary += `<p>Vinyl Type: ${vinylType}</p>`;
 summary += `<p>Vinyl Cost: £${cost.toFixed(2)}</p>`;
-summary += `<h4>Printed Parts: £${labelTotal.toFixed(2)}</h4>`;
+summary += `<p>Label Cost: £${labelTotal.toFixed(2)}</p>`;
+summary += `<p>Inners Cost: £${innersTotal.toFixed(2)}</p>`;
+summary += `<p>Outers Cost: £${outersTotal.toFixed(2)}</p>`;
     
     
     
@@ -138,13 +531,21 @@ summary += `<h4>Printed Parts: £${labelTotal.toFixed(2)}</h4>`;
   }
   
   if (hasPrintedLabel) {
-    totalPrintedPartsCost += 100;
+    printingFee += 100;
+  }
+  if (hasPrintedInner) {
+    printingFee += 100;
+  }
+  if (hasPrintedOuter) {
+    printingFee += 100;
   }
 
   let effectiveSetupFee = setupFee;
   const grandTotal = priceTotal + effectiveSetupFee;
+  
   summary += `<p>Setup Fee: £${effectiveSetupFee.toFixed(2)}</p>`;
   summary += `<h4>Printed Parts: £${totalPrintedPartsCost.toFixed(2)}</h4>`;
+  summary += `<p>Printing Fee: £${printingFee.toFixed(2)}</p>`;
   summary += `<h2>Total: £${grandTotal.toFixed(2)}</h2>`;
 
 
--- functions/2printed-parts.js
+++ functions/2printed-parts.js
@@ -19,6 +19,66 @@ function printedParts() {
             <option value="Printed">Printed (Dynamic)</option>
             <option value="Supplied">Supplied (£0.10)</option>
           </select>
+
+
+          <label>Inner Sleeve</label>
+        <select class="variantInner">
+          <option value="None">None</option>
+          <option value="Black Gloss Disco 0 Holes">Black Gloss Disco 0 Holes (£0.35)</option>
+          <option value="Black Gloss Disco 2 Holes">Black Gloss Disco 2 Holes (£0.35)</option>
+          <option value="Black Matt Disco 0 Holes">Black Matt Disco 0 Holes (£0.35)</option>
+          <option value="Black Matt Disco 2 Holes">Black Matt Disco 2 Holes (£0.35)</option>
+          <option value="White Gloss Disco 0 Holes">White Gloss Disco 0 Holes (£0.35)</option>
+          <option value="White Gloss Disco 2 Holes">White Gloss Disco 2 Holes (£0.35)</option>
+          <option value="White Matt Disco 0 Holes">White Matt Disco 0 Holes (£0.35)</option>
+          <option value="White Matt Disco 2 Holes">White Matt Disco 2 Holes (£0.35)</option>
+          <option value="Kraft Disco 0 Holes">Kraft Disco 0 Holes (£0.35)</option>
+          <option value="Kraft Disco 2 Holes">Kraft Disco 2 Holes (£0.35)</option>
+          <option value="Black Paper">Black Paper (£0.22)</option>
+          <option value="White Paper">White Paper (£0.21)</option>
+          <option value="Black Poly">Black Poly (£0.25)</option>
+          <option value="White Poly">White Poly (£0.23)</option>
+          <option value="Rounded Anti-Static">Rounded Anti-Static (£0.20)</option>
+
+          <option value="Printed">Printed (Dynamic)</option>
+
+          <option value="Supplied">Supplied (£0.10)</option>
+        </select>
+
+
+        <label>Outer Sleeve</label>
+        <select class="variantOuter" >
+          <option value="">None</option>
+          <option value="Black Gloss 0">Black Gloss 0 Holes (£0.35)</option>
+          <option value="Black Gloss 2">Black Gloss 2 Holes (£0.35)</option>
+          <option value="Black Matt 0">Black Matt 0 Holes (£0.35)</option>
+          <option value="Black Matt 2">Black Matt 2 Holes (£0.35)</option>
+          <option value="White Gloss 0">White Gloss 0 Holes (£0.35)</option>
+          <option value="White Gloss 2">White Gloss 2 Holes (£0.35)</option>
+          <option value="White Matt 0">White Matt 0 Holes (£0.35)</option>
+          <option value="White Matt 2">White Matt 2 Holes (£0.35)</option>
+          <option value="Kraft 0">Kraft 0 Holes (£0.35)</option>
+          <option value="Kraft 2">Kraft 2 Holes (£0.35)</option>
+          <option value="Black Gloss 3mm 0">Black Gloss 3mm 0 Holes (£0.39)</option>
+          <option value="Black Gloss 3mm 2">Black Gloss 3mm 2 Holes (£0.39)</option>
+          <option value="Black Matt 3mm 0">Black Matt 3mm 0 Holes (£0.39)</option>
+          <option value="Black Matt 3mm 2">Black Matt 3mm 2 Holes (£0.39)</option>
+          <option value="White Gloss 3mm 0">White Gloss 3mm 0 Holes (£0.39)</option>
+          <option value="White Gloss 3mm 2">White Gloss 3mm 2 Holes (£0.39)</option>
+          <option value="White Matt 3mm 0">White Matt 3mm 0 Holes (£0.39)</option>
+          <option value="White Matt 3mm 2">White Matt 3mm 2 Holes (£0.39)</option>
+          <option value="Kraft 3mm 0">Kraft 3mm 0 Holes (£0.39)</option>
+          <option value="Kraft 3mm 2">Kraft 3mm 2 Holes (£0.39)</option>
+
+          <option value="Printed 3mm">Printed 3mm Spine (Dynamic)</option>
+          <option value="Printed 5mm">Printed 5mm Spine (Dynamic)</option>
+          <option value="Gatefold">Printed Gatefold (Dynamic)</option>
+
+          <option value="Picture Disc">Picture Disc Sleeve (£0.70)</option>
+          <option value="Supplied">Supplied (£0.10)</option>
+        </select>
+
+       
         </div>
       `).join("")}
     </div>
